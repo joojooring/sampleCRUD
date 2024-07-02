d7f34ad28c7e14d3b41f31329c7ae2e1c12c22a8
Juree <jooryfgi.gmail.com>
2024-07-02 16:17:26 +0900

    test

---
+++ src/main/java/sample/spring/hjr/BoardController.java
@@ -0,0 +1,16 @@
+package sample.spring.hjr;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class BoardController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
