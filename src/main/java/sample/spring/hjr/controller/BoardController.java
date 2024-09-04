c862f39f5c2a39de0ab07da831c04aad0e5c0f27
Juree <ghkdwnfl0557@naver.com>
2024-09-04 18:51:07 +0900

    sql 로그 찍기

--- src/main/java/sample/spring/hjr/controller/BoardController.java
+++ src/main/java/sample/spring/hjr/controller/BoardController.java
@@ -2,6 +2,11 @@ package sample.spring.hjr.controller;
 
 import javax.inject.Inject;
 
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,43 +23,52 @@ import sample.spring.hjr.service.BoardService;
 @RequestMapping(value="/")
 public class BoardController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
 	@Autowired
 	private BoardService boardService;
 	
 	@RequestMapping(value="/listAll", method=RequestMethod.GET)
 	public void listAll(Model model) throws Exception {
+		logger.info("전체페이지");
 		model.addAttribute("list", boardService.listAll());
 	}
 	
 	@RequestMapping(value="/regist", method = RequestMethod.GET)
 	public void registGet(BoardVO board, Model model) throws Exception{
+		logger.info("등록페이지");
 		
 	}
 	
 	@RequestMapping(value="/regist", method = RequestMethod.POST)
 	public String registPost(BoardVO board, RedirectAttributes rttr) throws Exception{
+		logger.info("등록");
 		boardService.regist(board); // 글작성 service 호출
 		return "redirect:/listAll";
 	}
 	
 	@RequestMapping(value="/read", method=RequestMethod.GET)
 	public void read(@RequestParam("no")int no, Model model) throws Exception{
+		logger.info("읽기전용");
 		model.addAttribute(boardService.read(no));
 	}
 	
 	@RequestMapping(value="/modify", method=RequestMethod.GET)
 	public void modifyGET(int no, Model model) throws Exception{
+		logger.info("수정페이지");
 		model.addAttribute(boardService.read(no));
 	}
 	
 	@RequestMapping(value="/modify", method=RequestMethod.POST)
 	public String modify(BoardVO board, RedirectAttributes rttr) throws Exception{
+		logger.info("수정");
 		boardService.modify(board);
 		return "redirect:/listAll";
 	}
 	
 	@RequestMapping(value="/remove", method=RequestMethod.POST)
 	public String remove(@RequestParam("no") int no, RedirectAttributes rttr) throws Exception{
+		logger.info("삭제");
 		boardService.remove(no); // 서비스에서 글삭제관련 호출
 		return "redirect:/listAll";
 		
